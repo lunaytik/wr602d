5cbf25e547840c38a4797f54b40f71a9251c0692
lunaytik <nathandeuil13@gmail.com>
2024-06-12 11:34:24 +0200

    feat: add orderby createdAt for pdf collection

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -56,6 +56,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var Collection<int, Pdf>
      */
     #[ORM\OneToMany(targetEntity: Pdf::class, mappedBy: 'owner')]
+    #[ORM\OrderBy(['createdAt' => 'DESC'])]
     private Collection $pdfs;
 
     #[ORM\Column(nullable: true)]
